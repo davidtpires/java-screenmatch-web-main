6628f71c5fe53411cc017ec638735e9e0308b372
David T. <davidt.info@gmail.com>
2024-10-31 10:19:22 -0300

    Retornando os detalhes da série

--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,18 +26,21 @@ public class SerieService {
 
     private List<SerieDTO> converteDados(List<Serie> series){
         return series.stream()
-                .map(s -> new SerieDTO(s.getId(),
-                        s.getTitulo(),
-                        s.getTotalTemporadas(),
-                        s.getAvaliacao(),
-                        s.getGenero(),
-                        s.getAtores(),
-                        s.getPoster(),
-                        s.getSinopse()))
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());
     }
 
     public List<SerieDTO> obterLancamentos() {
         return converteDados(repositorio.encontrarEpisodiosMaisRecentes());
+    }
+
+    public SerieDTO obterPorId(Long id) {
+        Optional<Serie> serie = repositorio.findById(id);
+
+        if (serie.isPresent()){
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+        }
+        return null;
     }
 }
